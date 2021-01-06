6a43df82ee2efc30484876ba230d0e8d32c2eafc
Leandro Duarte <lduarte@unifei.edu.br>
2021-01-06 16:21:31 -0300

    #App1 Sorteio

---
+++ Sorteio/app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view){
+        TextView resultadoSorteio = findViewById(R.id.textViewResultado);
+        int numero = new Random().nextInt(11);
+        resultadoSorteio.setText("Número =  "+numero);
+    }
+}
